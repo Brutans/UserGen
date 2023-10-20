407ede61aa01dac704f0b24b8599acfff7801e01
Brutans <mrbrutans@gmail.com>
2023-10-20 13:12:53 +0300

    second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,47 +12,10 @@ public class App {
         List<User> usersM = new ArrayList<>();
         List<User> usersW = new ArrayList<>();
         UserGenerator token = new UserGenerator();
-
-//        //Добавление тестовых пользователей
-//        User user1 = new User("Denis", 19, "M");
-//        User user2 = new User("Kate", 18, "W");
-//        User user3 = new User("Alex", 17, "M");
-//        User user4 = new User("Anton", 19, "M");
-//        User user5 = new User("Banana", 21, "M");
-//        User user6 = new User("Vlad", 20, "M");
-//        User user7 = new User("Vika", 22, "W");
-//        User user8 = new User("Alex", 17, "W");
-//        User user9 = new User("Danya", 16, "M");
-//        User user10 = new User("Jane", 19, "W");
-//
-//
-//        //Добавление юзеров в лист
-//        users.add(user1);
-//        users.add(user2);
-//        users.add(user3);
-//        users.add(user4);
-//        users.add(user5);
-//        users.add(user6);
-//        users.add(user7);
-//        users.add(user8);
-//        users.add(user9);
-//        users.add(user10);
-
-
-//        //Вывод юзеров в общий список
-//        for (User user : users) {
-//            System.out.println("Имя: " + user.getName() + ", Возраст: " + user.getAge() + ", Пол: " + user.getGender());
-//        }
-//        //Вывод юзеров в список W
-//        for (User user : users) {
-//            if (user.gender.equals("W")) {
-//                usersW.add(user);
-//            } else {
-//                usersM.add(user);
-//            }
-//        }
-//        System.out.println(usersM.toString());
-//        System.out.println(usersW.toString());
+        //Переменное окружение
+        String envName = "SIMPLE_ENV";
+        String envValue = "Hello, World!25";
+        System.setProperty(envName, envValue);
 
         //Генератор строк из класса utils
         //Scanner number = new Scanner(System.in);
@@ -83,5 +46,9 @@ public class App {
         }
         System.out.println(usersM.toString());
         System.out.println(usersW.toString());
+
+        // Получение значения переменной окружения
+        String value = System.getProperty(envName);
+        System.out.println(envName + ": " + value);
     }
 }
